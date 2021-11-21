4ed0666757c305e3968d8d0c7d73044b60cd84be
Samuel Ebebenge <samuel.ebebenge@gmail.com>
2021-11-21 22:58:18 +0100

    adding obstacle system

--- lib/faster_game.dart
+++ lib/faster_game.dart
@@ -5,11 +5,14 @@ import 'package:faster/components/velocity_component.dart';
 import 'package:faster/systems/background_system.dart';
 import 'package:faster/systems/jump_system.dart';
 import 'package:faster/systems/sprite_system.dart';
+import 'package:faster/systems/obstacle_system.dart';
+
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/parallax.dart';
 import 'package:flame_oxygen/flame_oxygen.dart';
+import 'dart:math';
 
 class FasterGame extends OxygenGame with TapDetector {
   double speed = 10;
@@ -18,29 +21,37 @@ class FasterGame extends OxygenGame with TapDetector {
   Future<void> init() async {
     await Flame.device.fullScreen();
     await Flame.device.setLandscape();
+    Random random = new Random();
 
     world
       ..registerSystem(BackgroundSystem())
       ..registerSystem(SpriteSystem())
       ..registerSystem(JumpSystem())
+      ..registerSystem(ObstacleSystem())
       ..registerComponent<VelocityComponent, Vector2>(() => VelocityComponent())
       ..registerComponent<TapInputComponent, bool>(() => TapInputComponent())
-      ..registerComponent<ParallaxComponent, Parallax>(() => ParallaxComponent())
-      ..registerComponent<DifficultyComponent, int>(() => DifficultyComponent());
+      ..registerComponent<ParallaxComponent, Parallax>(
+          () => ParallaxComponent())
+      ..registerComponent<DifficultyComponent, int>(
+          () => DifficultyComponent());
 
     createEntity(
       name: 'Player',
       position: Vector2(50, world.game.size.y - 100),
       size: Vector2.all(64),
     )
-      ..add<SpriteComponent, SpriteInit>(SpriteInit(await loadSprite('character.png')))
+      ..add<SpriteComponent, SpriteInit>(
+          SpriteInit(await loadSprite('character.png')))
       ..add<VelocityComponent, Vector2>(
         Vector2(0, 150),
       )
       ..add<TapInputComponent, bool>(false);
 
     var parallax = await loadParallax(
-      [ParallaxImageData('background.png'), ParallaxImageData('groundGrass.png')],
+      [
+        ParallaxImageData('background.png'),
+        ParallaxImageData('groundGrass.png')
+      ],
       baseVelocity: Vector2(speed, 0),
       velocityMultiplierDelta: Vector2(1.8, 1.0),
     );
@@ -52,6 +63,18 @@ class FasterGame extends OxygenGame with TapDetector {
     )
       ..add<ParallaxComponent, Parallax>(parallax)
       ..add<DifficultyComponent, int>();
+
+    createEntity(
+      name: 'Obstacle',
+      position: Vector2(world.game.size.x - 50,
+          random.nextDouble() * (world.game.size.y) - 128),
+      size: Vector2.all(128),
+    )
+      ..add<SpriteComponent, SpriteInit>(
+          SpriteInit(await loadSprite('pizza.png')))
+      ..add<VelocityComponent, Vector2>(
+        Vector2(-200, 0),
+      );
   }
 
   @override
---
+++ lib/systems/obstacle_system.dart
@@ -0,0 +1,45 @@
+import 'package:faster/components/velocity_component.dart';
+import 'package:faster/faster_game.dart';
+import 'package:flame_oxygen/flame_oxygen.dart';
+import 'dart:async';
+
+class ObstacleSystem extends System with UpdateSystem, GameRef<FasterGame> {
+  Timer? timer;
+
+  Query? _query;
+
+  @override
+  void init() {
+    _query = createQuery([
+      Has<PositionComponent>(),
+      Has<VelocityComponent>(),
+      Has<SpriteComponent>(),
+    ]);
+  }
+
+  @override
+  void dispose() {
+    _query = null;
+    super.dispose();
+  }
+
+  @override
+  void update(double delta) {
+    Timer obstacleTimer = Timer.periodic(Duration(seconds: 5), (timer) {
+      // runs every 5 seconds
+    });
+    for (final entity in _query?.entities ?? <Entity>[]) {
+      final velocity = entity.get<VelocityComponent>()!.velocity;
+      var position = entity.get<PositionComponent>()!.position;
+      final screenSize = game!.size;
+      final size = entity.get<SizeComponent>()!.size;
+      final maxObstacleApparitionTime = 5; //sec
+
+      if (position.y + size.y <= screenSize.y) {
+        position.add((velocity * delta));
+      } else {
+        // TODO : Destroy the obstacle
+      }
+    }
+  }
+}
