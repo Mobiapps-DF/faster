e46053bdb2bcd55c857d3fa84c062a85735bb72b
malrok <themalrok@gmail.com>
2021-12-28 12:21:54 +0100

    fixed player always jumping after dying if it was jumping then

--- lib/utils/game_status_helper.dart
+++ lib/utils/game_status_helper.dart
@@ -1,8 +1,12 @@
 import 'package:faster/components/difficulty_component.dart';
 import 'package:faster/components/game_status_component.dart';
+import 'package:faster/components/tap_input_component.dart';
+import 'package:faster/components/velocity_component.dart';
 import 'package:faster/entities/game_session_entity.dart';
 import 'package:faster/entities/obstacle_entity.dart';
+import 'package:faster/entities/player_entity.dart';
 import 'package:faster/faster_game.dart';
+import 'package:flame/game.dart';
 import 'package:flame_oxygen/flame_oxygen.dart';
 
 bool isPlaying(FasterGame game) =>
@@ -22,6 +26,12 @@ setPlaying(FasterGame game) {
     gameEntity.get<GameStatusComponent>()?.status = GameStatus.playing;
 
     game.world.entityManager.processRemovedEntities();
+  }
+  Entity? entity = game.world.entityManager.getEntityByName(playerEntity);
+  if (entity != null) {
+    entity.get<VelocityComponent>()!.reset();
+    entity.get<PositionComponent>()!.position.setFrom(Vector2(50, game.world.game.size.y - playerSizeY));
+    entity.get<TapInputComponent>()!.value = false;
   }
 }
 
