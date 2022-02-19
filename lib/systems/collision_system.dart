aad13510be08229b91b6af410b3d8d6fcf490640
malrok <themalrok@gmail.com>
2022-02-19 14:09:43 +0100

    added an animation after player's death

--- lib/systems/collision_system.dart
+++ lib/systems/collision_system.dart
@@ -6,6 +6,7 @@ import 'package:faster/entities/game_session_entity.dart';
 import 'package:faster/entities/player_entity.dart';
 import 'package:faster/faster_game.dart';
 import 'package:faster/utils/game_status_helper.dart';
+import 'package:flame/game.dart';
 import 'package:flame_oxygen/flame_oxygen.dart';
 
 class CollisionSystem extends System with UpdateSystem, GameRef<FasterGame> {
@@ -40,6 +41,7 @@ class CollisionSystem extends System with UpdateSystem, GameRef<FasterGame> {
         if (entity.get<HitBoxComponent>()!.value!.overlaps(playerHitBox)) {
           final animatedSprites = _player!.get<AnimatedSpritesComponent>()!;
           animatedSprites.activeAnimation = deathAnimation;
+          _player?.get<VelocityComponent>()!.velocity = Vector2.zero();
 
           game!.world.entityManager.getEntityByName(gameSessionEntity)!.get<GameStatusComponent>()!.status =
               GameStatus.dead;
